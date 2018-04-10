bda267d052ccc3737d0bb5b8057b4dc1342d6514
dockerlearn <36786300+dockerlearn@users.noreply.github.com>
2018-04-10 07:26:18 -0400

    Initial commit

---
+++ SimpleCalculator/Calculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class Calculator
+    {
+        public static double Calculate(double firstNum, double secondNum, string op)
+        {
+            double result;
+            switch (op)
+            {
+                case "+":
+                    result = firstNum + secondNum;
+                    break;
+                case "-":
+                    result = firstNum - secondNum;
+                    break;
+                case "*":
+                    result = firstNum * secondNum;
+                    break;
+                case "/":
+                    result = firstNum / secondNum;
+                    break;
+                default:
+                    throw new NotSupportedException("Specified operation is not supported");
+            }
+            return result;
+        }
+    }
+
+    public static class CalculatorExtension
+    {
+
+        public static double Getresult(this System.String op,double firstNum, double secondNum)
+        {
+            double result;
+            switch (op)
+            {
+                case "add":
+                    result = firstNum + secondNum;
+                    break;
+                case "subtract":
+                    if(firstNum>secondNum)
+                   result = firstNum - secondNum;
+                    else
+                        result = secondNum - firstNum;
+                    break;
+                case "multiply":
+                    result = firstNum * secondNum;
+                    break;
+                case "divide":
+                    if (firstNum > secondNum)
+                    result = firstNum / secondNum;
+                    else
+                        result = secondNum / firstNum;
+                    break;
+                default:
+                    throw new NotSupportedException("Specified operation is not supported");
+            }
+            return result;
+        }
+    }
+}
---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the first Number");
+                double firstNumber = InputFormatter.FormatNumber(Console.ReadLine());
+
+                Console.WriteLine("Enter the second Number");
+                double secondNumber = InputFormatter.FormatNumber(Console.ReadLine());
+
+                Console.WriteLine("Enter the operator");
+                //string op = InputFormatter.FormatOperator(Console.ReadLine());
+                string op = Console.ReadLine();
+
+                //double result = Calculator.Calculate(firstNumber, secondNumber, op);
+                Console.WriteLine(op.Getresult(firstNumber,secondNumber));
+                //Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Exception encountered while performing calculation. Error: {0}", ex.Message));
+            }
+            finally {
+                Console.ReadKey();
+            }
+        }
+    }
+}
